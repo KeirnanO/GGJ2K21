58da076f31bdb05075d5f78b8251f70be9736bfb
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2021-01-29 22:53:29 -0500

    Shit

---
+++ Paranoia/Assets/Scripts/LanternController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour
+{
+    public Transform target;
+    
+    //bool moving;
+
+    List<Transform> waypoints = new List<Transform>();
+    int currWaypoint = -1;
+
+    public float lerpTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] waypointArray = GameObject.FindGameObjectsWithTag("Point");
+
+        foreach(GameObject o in waypointArray)
+        {
+            waypoints.Add(o.transform);
+        }
+    }
+
+   IEnumerator Move()
+    {
+        currWaypoint++;
+        target = waypoints[currWaypoint];
+        
+        while((transform.position - target.position).magnitude > 0.5f)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, target.position, lerpTime);
+            transform.position = newPosition;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            if (currWaypoint < waypoints.Count)
+            {
+                StopAllCoroutines();
+                StartCoroutine(Move());
+            }
+        }
+    }
+
+}
---
+++ Paranoia/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    protected CharacterController controller;
+    public float speed;
+
+    protected Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //velocity += moveDirection.normalized * acceleration * Time.deltaTime;
+
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, Camera.main.transform.eulerAngles.y, transform.rotation.z);
+        //Quaternion newRotation = new Quaternion(transform.rotation.x, Camera.main.transform.rotation.y, transform.rotation.z, transform.rotation.w);
+        //transform.rotation = newRotation;
+    }
+}
