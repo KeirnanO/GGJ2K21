cabd991685d9a220a0bf727dc35964d3faa5d507
chasenater23 <chase.johnson@hotmail.com>
2021-01-30 19:51:34 -0500

    Character model stuff

---
+++ Paranoia/Assets/Character Model/enemymover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymover : MonoBehaviour
+{
+
+    public GameObject player;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(player.transform);
+        Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
+        transform.position += transform.forward * 1f * Time.deltaTime;
+
+    }
+}
