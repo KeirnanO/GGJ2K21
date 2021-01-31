4b011f655c91b1fcf6794b3756cecdd725768d48
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2021-01-31 15:52:15 -0500

    Finished

---
+++ Paranoia/Assets/Scripts/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public bool gameOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
