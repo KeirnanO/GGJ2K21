f6fd7e7bfcce08c931aa695ebff87158d90b0a7e
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2021-01-31 14:29:54 -0500

    Fix

---
+++ Paranoia/Assets/LanternSoundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternSoundController : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip[] clips;
+
+    int num = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        StartCoroutine(Creak());
+    }
+
+    IEnumerator Creak()
+    {
+        while (true)
+        {
+            audioSource.PlayOneShot(clips[num]);
+
+            num++;
+            num %= 2;
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
